41bc3cb16aae0686052834b3791c7b1a92a21067
unknown <andrewpaulcummins@gmail.com>
2026-04-09 23:40:31 +0100

    Changed Runner tags

--- src/test/java/com/andrewcummins/framework/runners/RunCukesTest.java
+++ src/test/java/com/andrewcummins/framework/runners/RunCukesTest.java
@@ -51,7 +51,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 )
 @ConfigurationParameter(
         key = FILTER_TAGS_PROPERTY_NAME,
-        value = "@api"
+        value = "@regression and @sanity"
 )
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
